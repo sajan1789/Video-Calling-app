355598997c28e67e403176aa150e148491413e06
Sajan Kumar <sajankumar@Sajans-MacBook-Air.local>
2024-02-06 15:35:53 +0530

    calling-complete

---
+++ my-app/src/Components/Home.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState ,} from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Home.css'
+const Home = () => {
+const [roomId,setRoomId]=useState("")
+const navigate=useNavigate()
+const handleJoin=(e)=>{
+    e.preventDefault()
+    navigate(`/room/${roomId}`)
+}
+
+  return (
+    <div className="container">
+  
+    <form onSubmit={handleJoin} className="form">
+      <input
+        type="text"
+        value={roomId}
+        placeholder="Enter Room Id"
+        onChange={(e) => setRoomId(e.target.value)}
+        required
+        className="input"
+      />
+      <button type="submit" className="button">Join</button>
+    </form>
+  </div>
+  )
+}
+
+export default Home
---
+++ my-app/src/Components/Room.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+const Room = () => {
+const {roomID}=useParams()
+const myMeeting=async(element)=>{
+    const appID = 1719582741;
+      const serverSecret ="96fa806f69102dab1796987b0d18ea24"
+      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,Date.now().toString(),"Sajan");     
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      zp.joinRoom({
+        container: element,
+        sharedLinks: [
+          {
+            name: 'Copy link',
+            url:
+             window.location.protocol + '//' + 
+             window.location.host + window.location.pathname +
+              '?roomID=' +
+              roomID,
+          },
+        ],
+        scenario: {
+          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+        },
+        
+      });
+}
+    
+  return (
+    <div ref={myMeeting}  style={{ width: '100vw', height: '100vh' }}>
+
+    </div>
+  )
+}
+
+export default Room
